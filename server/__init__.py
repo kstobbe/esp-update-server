52b9842e056d34ff42dbbd364e3242d566bd627c
Marco <marcovannoord@users.noreply.github.com>
2021-12-31 12:28:58 +0100

    fix login system

--- server/__init__.py
+++ server/__init__.py
@@ -1,18 +1,30 @@
+# init.py
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_moment import Moment
+from flask_login import LoginManager 
 
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    moment = Moment(app)
 
-    app.config['SECRET_KEY'] = 'secret-key-goes-here'
+    app.config['SECRET_KEY'] = 'SECRET_KEY_HERE'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
 
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
@@ -21,6 +33,5 @@ def create_app():
     # blueprint for non-auth parts of app
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    print("Running create_app")
 
     return app
